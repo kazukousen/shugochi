3a947df226672d7de362643943f740022bb75ea0
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-03-31 14:17:56 +0900

    typo

--- 4/searchengine.py
+++ 4/searchengine.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 import urllib2
 from bs4 import *
 from urlparse import urljoin
@@ -38,40 +39,37 @@ class crawler:
     return False
 
   # 2つのページの間にリンクを付け加える
-  def addlinkref(self. urlFrom, urlTo, linkText):
-    pass
-
-  # ページのリストを受け取り、与えられた深さで幅優先の検索を行い
-  # ページをインデクシングする
-  def crawl(self, pages, depth=2):
+  def addlinkref(self, urlFrom, urlTo, linkText):
     pass
 
   # データベースのテーブルを作る
   def createindextables(self):
    pass
 
+  # ページのリストを受け取り、与えられた深さで幅優先の検索を行い
+  # ページをインデクシングする
   def crawl(self, pages, depth=2):
     for i in range(depth):
       newpages=set()
-        for page in pages:
-          try:
-            c=urllib2.urlopen(page)
-          except:
-            print "Could not open %s" % page
-            continue
-          soup = BeautifulSoup(c.read())
-          self.addtoindex(page, soup)
+      for page in pages:
+        try:
+          c=urllib2.urlopen(page)
+        except:
+          print "Could not open %s" % page
+          continue
+        soup = BeautifulSoup(c.read())
+        self.addtoindex(page, soup)
 
-          links = soup('a')
-          for link in links:
-            if ('href' in dict(link.attrs)):
-              url = urljoin(page, link['href'])
-              if url.find("'")!=-1: continue
-                url = url.split("#")[0] # アンカーを取り除く
-              if url[0:4]=='http' and not self.isindexed(url):
-                newpages.add(url)
-              linkText = self.gettextonly(link)
-              self.addlinkref(page, url, linkText)
+        links = soup('a')
+        for link in links:
+          if ('href' in dict(link.attrs)):
+            url = urljoin(page, link['href'])
+            if url.find("'")!=-1: continue
+            url = url.split("#")[0] # アンカーを取り除く
+            if url[0:4]=='http' and not self.isindexed(url):
+              newpages.add(url)
+            linkText = self.gettextonly(link)
+            self.addlinkref(page, url, linkText)
 
-          self.dbcommit()
-        pages = newpages
+        self.dbcommit()
+      pages = newpages
